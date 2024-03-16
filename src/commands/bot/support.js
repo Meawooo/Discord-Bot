6e419c8409c9ef11d4af5f0deaac5e3014bc0191
Meawooo <96882676+Meawooo@users.noreply.github.com>
2024-03-16 23:00:29 +0530

    Update support.js

--- src/commands/bot/support.js
+++ src/commands/bot/support.js
@@ -12,7 +12,7 @@ module.exports = async (client, interaction, args) => {
     client.embed({
         title: `❓・Support`,
         desc: `Make your server even better with Bot!`,
-        image: "https://cdn.discordapp.com/attachments/843487478881976381/874694194474668052/Bot_banner_invite.jpg",
+        image: "https://cdn.discordapp.com/attachments/875968296556572722/1218412249564184726/clomel.gif?ex=660791d5&is=65f51cd5&hm=d9c1e7ba3b6960c48c3fe0c68acb7ed319b0ae069d608998d9f4e154885bb4e2&",
         url: client.config.discord.serverInvite,
         components: [row],
         type: 'editreply'
